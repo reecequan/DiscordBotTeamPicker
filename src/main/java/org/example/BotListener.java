4811d8200445816def98b93ef18627a01d11749c
reecequan <aog.chain@googlemail.com>
2021-01-06 16:41:39 +0000

    Adding stuff

---
+++ src/main/java/org/example/BotListener.java
@@ -0,0 +1,121 @@
+package org.example;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class BotListener extends ListenerAdapter {
+    private java.util.List<String> names = new ArrayList<>();
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent e)
+    {
+        String prompt = e.getMessage().getContentRaw().split(" ")[0];
+
+        String input = e.getMessage().getContentRaw().replace(prompt,"").trim();
+        switch (prompt){
+            case "!addName":
+                addNames(input, e);
+                break;
+            case "!removeName":
+                removeUser(input, e);
+                break;
+            case "!showUsers":
+                e.getChannel().sendMessage(names.toString()).queue();
+                break;
+            case "!teamCreate":
+                createTeam(e);
+                break;
+            case "!teamNoRoles":
+                createTeamNoRole(e);
+                break;
+            case "!clearTeam":
+                names.clear();
+                e.getChannel().sendMessage("All names have been cleared").queue();
+                break;
+            case "!teamHelp":
+                getHelp(e);
+                break;
+        }
+    }
+
+    private void addNames(String input, MessageReceivedEvent e)
+    {
+        String inputStrings[] = input.split(" ");
+        for(String name : inputStrings)
+        {
+            if(names.size() < 10){
+                names.add(name.trim());
+                e.getChannel().sendMessage(name.trim() + " has been added").queue();
+            } else
+            {
+                e.getChannel().sendMessage("Unable to add " + name + " as list is at capacity").queue();
+            }
+        }
+    }
+
+    private void createTeam(MessageReceivedEvent e)
+    {
+        if(names.size() != 10)
+        {
+            e.getChannel().sendMessage("There is not enough names in the list to create teams").queue();
+            return;
+        }
+        e.getChannel().sendMessage(new TeamPicker().splitTeamsAndAssignRoles(names)).queue();
+    }
+
+    private void createTeamNoRole(MessageReceivedEvent e)
+    {
+        if(names.size() != 10)
+        {
+            e.getChannel().sendMessage("There is not enough names in the list to create teams").queue();
+            return;
+        }
+        e.getChannel().sendMessage(new TeamPicker().splitTeams(names)).queue();
+    }
+
+    private void getHelp(MessageReceivedEvent e)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("```");
+        sb.append("!addName - adds names to a list to be able to create a team");
+        sb.append(System.getProperty("line.separator"));
+        sb.append(System.getProperty("line.separator"));
+
+        sb.append("!removeName - removes names from the list");
+        sb.append(System.getProperty("line.separator"));
+        sb.append(System.getProperty("line.separator"));
+
+        sb.append("!teamCreate - once the team list is at 10 you can create 2 random league teams");
+        sb.append(System.getProperty("line.separator"));
+        sb.append(System.getProperty("line.separator"));
+
+        sb.append("!teamNoRoles - Splits the names into 2 teams");
+        sb.append(System.getProperty("line.separator"));
+        sb.append(System.getProperty("line.separator"));
+
+        sb.append("!clearTeam - removes all names from the team list");
+        sb.append(System.getProperty("line.separator"));
+
+        sb.append("```");
+        e.getChannel().sendMessage(sb.toString()).queue();
+    }
+
+    private void removeUser(String input, MessageReceivedEvent e)
+    {
+        String inputStrings[] = input.split(" ");
+        for(String name : inputStrings)
+        {
+            if(names.remove(name)){
+                e.getChannel().sendMessage(name.trim() + " has been removed").queue();
+            } else
+            {
+                e.getChannel().sendMessage("Unable to remove " + name + " as not found in the list").queue();
+            }
+        }
+    }
+
+
+}
---
+++ src/main/java/org/example/TeamPicker.java
@@ -0,0 +1,104 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TeamPicker {
+    StringBuilder sb = new StringBuilder();
+
+
+    private List<String> getRoles()
+    {
+        List<String> roles = new ArrayList();
+        roles.add("Top");
+        roles.add("Jungle");
+        roles.add("Mid");
+        roles.add("ADC");
+        roles.add("Support");
+        return new ArrayList<>(roles);
+    }
+
+    private void assignTeam(List<String> names)
+    {
+        List<String> roles = getRoles();
+        for (String name : names)
+        {
+            String role = roles.get(0);
+            sb.append(name.trim() + " has been assigned " + role);
+            sb.append(System.getProperty("line.separator"));
+            roles.remove(role);
+        }
+    }
+
+    protected String splitTeams(List<String> allNames)
+    {
+        List <String> teamOne = new ArrayList<>();
+        List <String> teamTwo = new ArrayList<>();
+        Collections.shuffle(allNames);
+        for(int i=0; i < allNames.size(); i++)
+        {
+            if(i % 2 == 0)
+            {
+                teamOne.add(allNames.get(i));
+            }
+            else
+            {
+                teamTwo.add(allNames.get(i));
+            }
+        }
+        sb.append("```");
+        sb.append("--------------");
+        sb.append("Team One");
+        sb.append("--------------");
+        sb.append(System.getProperty("line.separator"));
+        for (String name: teamOne)
+        {
+            sb.append(name);
+            sb.append(System.getProperty("line.separator"));
+        }
+        sb.append("--------------");
+        sb.append("Team Two");
+        sb.append("--------------");
+        sb.append(System.getProperty("line.separator"));
+        for (String name: teamTwo)
+        {
+            sb.append(name);
+            sb.append(System.getProperty("line.separator"));
+        }
+        sb.append("```");
+        return sb.toString();
+    }
+
+    protected String splitTeamsAndAssignRoles(List<String> allNames)
+    {
+        List <String> teamOne = new ArrayList<>();
+        List <String> teamTwo = new ArrayList<>();
+        Collections.shuffle(allNames);
+        for(int i=0; i < allNames.size(); i++)
+        {
+            if(i % 2 == 0)
+            {
+                teamOne.add(allNames.get(i));
+            }
+            else
+            {
+                teamTwo.add(allNames.get(i));
+            }
+        }
+        sb.append("```");
+        sb.append("--------------");
+        sb.append("Team One");
+        sb.append("--------------");
+        sb.append(System.getProperty("line.separator"));
+        assignTeam(teamOne);
+        sb.append("--------------");
+        sb.append("Team Two");
+        sb.append("--------------");
+        sb.append(System.getProperty("line.separator"));
+        assignTeam(teamTwo);
+        sb.append("```");
+        return sb.toString();
+    }
+
+}
